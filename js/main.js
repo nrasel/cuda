f79a48e56182a280249dca37d163a9cb834f58f9
nrasel <nrasel452@gmail.com>
2021-07-23 12:09:45 +0600

    added all site

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+
+//sticky menu
+
+
+
+$(".js--services-section").waypoint(function(direction){
+    if(direction == "down"){
+        $("nav").addClass("sticky");
+    }else{
+        $("nav").removeClass("sticky");
+    }
+});
+
+
+
+
+
+// mixitup
+
+     var mixer = mixitup('.container')
+})
+
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width="0";
+}
+
